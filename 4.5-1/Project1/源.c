17a68d8987d6e3924bf46cce0c12c4ba957c1612
血溅黄沙 <3212138067@qq.com>
2025-10-23 22:17:13 +0800

    Add files via upload

---
+++ 4.5-1/Project1/源.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(void)
+{
+	char Fname[40];
+	char Lname[40];
+	scanf("%s %s",Lname, Fname);
+	printf("%s %s", Fname, Lname);
+
+	return 0;
+}
